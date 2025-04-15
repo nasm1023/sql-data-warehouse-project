10ff7ce2bef333a3d2f38df39224143519528c05
nasm1023 <127742124+nasm1023@users.noreply.github.com>
2025-04-15 08:35:48 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,63 +1,42 @@
---This scripts create the database, and if the database is existed then drop it and recreate a new one.
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
 
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_cust_info;
-CREATE TABLE bronze.crm_cust_info (
-	cst_id INT,
-	cst_key nvarchar(50),
-	cst_firstname nvarchar(50),
-	cst_lastname nvarchar(50),
-	cst_marital_status char(1),
-	cst_gndr char(1),
-	cst_create_date date
-);
+USE master;
+GO
 
-IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_prd_info;
-CREATE TABLE bronze.crm_prd_info (
-	prd_id INT,
-	prd_key nvarchar(50),
-	prd_nm nvarchar(50),
-	prd_cost int,
-	prd_line nvarchar(50),
-	prd_start_dt date,
-	prd_end_dt date
-);
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 
-IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_sales_details;
-CREATE TABLE bronze.crm_sales_details (
-	sls_ord_num nvarchar(50),
-	sls_prd_key nvarchar(50),
-	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
-	sls_sales int,
-	sls_quantity INT,
-	sls_price int
-);
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
 
-IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_cust_az12;
-CREATE TABLE bronze.erp_cust_az12(
-	cid nvarchar(50),
-	bdate date,
-	gen nvarchar(50)
-);
+USE DataWarehouse;
+GO
 
-IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_loc_a101;
-CREATE TABLE bronze.erp_loc_a101(
-	cid nvarchar(50),
-	cntry nvarchar(50)
-);
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
 
-IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_px_cat_g1v2;
-CREATE TABLE bronze.erp_px_cat_g1v2(
-	id nvarchar(50),
-	cid nvarchar(50),
-	subcat nvarchar(50),
-	maintenance nvarchar(50)
-);
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
